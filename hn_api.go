0788b1986c4ae656b909068dbc47bfb2ceb7ac9b
TaigaMikami <mikamimikami60@gmail.com>
2019-12-20 01:23:18 +0900

    use gourutine

--- hn_api.go
+++ hn_api.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/k0kubun/pp"
 	"github.com/otiai10/opengraph"
+
+	//"github.com/otiai10/opengraph"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type HackerNews struct {
@@ -16,6 +20,35 @@ type HackerNews struct {
 	Type        string `json:"type"`
 	Url         string `json:"url"`
 	Description string
+}
+
+func GetHackerNewsDetail(ids []int) []HackerNews {
+	wg := new(sync.WaitGroup)
+	var hns []HackerNews
+	var chn = make(chan HackerNews)
+	for _, s := range ids {
+		wg.Add(1)
+		url := "https://hacker-news.firebaseio.com/v0/item/" + strconv.Itoa(s) + ".json?print=pretty"
+		go func(url string) {
+			var hn HackerNews
+			res, _ := http.Get(url)
+			body, _ := ioutil.ReadAll(res.Body)
+			json.Unmarshal(body, &hn)
+			if hn.Url != "" {
+				og, err := opengraph.Fetch(hn.Url)
+				if err != nil {
+					log.Fatal(err)
+				}
+				hn.Description = og.Description
+			}
+			chn <- hn
+			wg.Done()
+		}(url)
+		hns = append(hns, <-chn)
+	}
+	pp.Print(hns)
+	wg.Wait()
+	return hns
 }
 
 func GetHackerNews(n int) []HackerNews {
@@ -31,25 +64,7 @@ func GetHackerNews(n int) []HackerNews {
 	var idHn []int
 	json.Unmarshal(body, &idHn)
 
-	var hns []HackerNews
-	var hn HackerNews
-	cnt := 0
-	for _, s := range idHn {
-		if cnt > n-1 {
-			break
-		}
-		url := "https://hacker-news.firebaseio.com/v0/item/" + strconv.Itoa(s) + ".json?print=pretty"
-		res, _ := http.Get(url)
-		body, _ := ioutil.ReadAll(res.Body)
-		json.Unmarshal(body, &hn)
-		og, err := opengraph.Fetch(hn.Url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		hn.Description = og.Description
-		hns = append(hns, hn)
-		cnt += 1
-	}
-
+	//var hns []HackerNews
+	hns := GetHackerNewsDetail(idHn[0 : n-1])
 	return hns
 }
